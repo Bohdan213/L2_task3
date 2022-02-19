27efdb436c8e6e4a2aea26c3ace25c81e0124959
Bohdan213 <bogdanpeleh213@gmail.com>
2022-02-19 21:19:48 +0200

    Update map_location_API.py

--- my_web_app/map_location_API.py
+++ my_web_app/map_location_API.py
@@ -7,14 +7,12 @@ import requests
 
 def get_json_from_API_IP(user_name):
     return requests.get(f"https://api.twitter.com/2/users/by/username/{user_name}", headers={
-        'Authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAAI7vZAEAAAAAWxLmcJ3L6Qn49oId3kUP5CZijmk'
-                         '%3DLDuIqIpBahgDanLhSxqwl7A2ueKRfXsFK4ll4zCgatORTveens'}).json()['data']['id']
+        'Authorization': 'Bearer {API}}'}).json()['data']['id']
 
 
 def get_json_from_API_friends(ID):
     return requests.get(f"https://api.twitter.com/2/users/{ID}/following?user.fields=location",
-                        headers={'Authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAAI7vZAEAAAAAWxLmcJ3L6Qn49oId3kUP5CZijmk%3'
-                                                  'DLDuIqIpBahgDanLhSxqwl7A2ueKRfXsFK4ll4zCgatORTveens'}).json()
+                        headers={'Authorization': 'Bearer {API}'}).json()
 
 
 def Main(users_name, count):
